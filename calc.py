b08b6e5e4706241b509a11cc698a01638409730c
Tiffany <Tiffany.tan1@monash.edu>
2023-04-18 10:32:35 +0800

    add division

--- calc.py
+++ calc.py
@@ -9,7 +9,7 @@ def compute(expression):
     elif operator == '-':
         return num0 - num1
     elif operator == '/':
-        return num0 * num1
+        return num0 / num1
     else:
         print('unknown operator!')
         return None
